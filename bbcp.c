77d89d0562791865d65188664aa21b9a672ad6ba
SiddharthChoudhary <firesid12@gmail.com>
2019-09-16 00:56:25 -0400

    Perfection

--- bbcp.c
+++ bbcp.c
@@ -7,11 +7,12 @@
 #include<string.h>
 #include<sys/stat.h>
 #define EXIT_ERROR_CODE -1
-#define BUFFERSIZE 1024
+#define BUFFERSIZE 16384
 
 int main(int argc,char *argv[]){
     char buffer[BUFFERSIZE];
     struct stat buf,secondDirectory;
+    int n,writeDescriptor,filedesc;
     /* if it's less than 2 parameters */
     if(argc<=2){
         fprintf(stderr,"Not sufficient parameters");
@@ -20,7 +21,6 @@ int main(int argc,char *argv[]){
     stat(argv[1],&buf);
     stat(argv[2],&secondDirectory);
 
-    int n = 0;
     if(S_ISDIR(buf.st_mode)){
         printf("Source is a directory\n");
         exit(1);
@@ -38,18 +38,17 @@ int main(int argc,char *argv[]){
             fprintf(stderr,"Symbolic link");
             exit(1);
         }
-        int filedesc = open(source,O_RDONLY);
-        if(filedesc ==-1){
+        if((filedesc = open(source,O_RDONLY)) ==-1){
             fprintf(stderr,"Unable to open %s",strerror(errno));
             exit(1);
         };
-        int writeDescriptor;
         if((writeDescriptor=open(destination,O_WRONLY|O_CREAT|O_TRUNC))==-1){
             fprintf(stderr,"Unable to write %s",strerror(errno));
             exit(1);
         };
 
         while((n = read(filedesc,buffer,BUFFERSIZE))>0){
+            printf("Printing a line with buffer size 16384\n");
             if(write(writeDescriptor,buffer,n)!=n){
                     fprintf(stderr,"Error");
                     exit(1);
